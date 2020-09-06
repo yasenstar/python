d1578080d7309a7bfa8d48c423e5d9380947131c
yasenstar <xiaoqizhao@outlook.com>
2020-09-05 20:19:31 -0400

    update 3body

--- 三体/3body-sim.py
+++ 三体/3body-sim.py
@@ -12,7 +12,7 @@ FPS   = 50   # 帧率
 exact = 50   # 每帧计算几次，次数越多越精确，它是动态变化的
 
 pygame.init()  # 初始化pygame
-screen = pygame.display.set_mode((640,480), pygame.DOUBLEBUF, 32)
+screen = pygame.display.set_mode((1024,768), pygame.DOUBLEBUF, 32)
 pygame.display.set_caption("模拟三体")
 clock = pygame.time.Clock()
 font = pygame.font.SysFont("Arial", 16)
@@ -23,7 +23,7 @@ class Star():
     vx, vy = 0, 0  # x,y方向上的速度
     ax, ay = 0, 0  # x,y方向上的加速度
     m = 0  # 质量
-    r = 10  # 半径
+    r = 20  # 半径
 
     def __init__(self, x, y, vx, vy, m):
         self.x = x
@@ -50,26 +50,63 @@ class Star():
         else:
             self.ay += a * _y
         
-        def run(self, time):
-            '''
-            计算time时间后的位置
-            :param time: 时间， 秒
-            :return:
-            '''
-            self.ax /= self.m
-            self.ay /= self.m
-            self.vx += self.ax * time
-            self.vy += self.ay * time
-            self.x += self.vx * time
-            self.y += self.vy * time
+    def run(self, time):
+        '''
+        计算time时间后的位置
+        :param time: 时间， 秒
+        :return:
+        '''
+        self.ax /= (self.m * 2)
+        self.ay /= self.m
+        self.vx += self.ax * time * 10
+        self.vy += self.ay * time
+        self.x += self.vx * time
+        self.y += self.vy * time
 
 # 设置三个星球
 star_list = []
 dd = 0.00001
-star_list.append(Star(200, 300, -30, -math.sqar(3)*30, 1000))
+star_list.append(Star(200, 300, -30, -math.sqrt(3)*30, 1000))
 star_list.append(Star(400, 300, -30, math.sqrt(3)*30, 1000))
 star_list.append(Star(300, 300-math.sqrt(3)*100+dd, 60, 0, 1000))
+""" star_list.append(Star(500, 300-math.sqrt(3)*100+dd, 60, 0, 1000))
+star_list.append(Star(100, 300, -30, -math.sqrt(3)*30, 1000))
+star_list.append(Star(300, 300, -30, math.sqrt(3)*30, 1000))
+star_list.append(Star(600, 300-math.sqrt(3)*100+dd, 60, 0, 1000))
+star_list.append(Star(350, 300-math.sqrt(3)*100+dd, 60, 0, 1000)) """
 
+# 主循环
+# 计算引力加速度
+def set_a(star_list):
+    for i, star in enumerate(star_list):
+        star.ax, star.ay = 0, 0
+        for j, other_star in enumerate(star_list):
+            if i != j:
+                star.set_a(other_star)
 
+# 游戏主循环
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            # 接收到退出时间后退出程序
+            exit()
+
+    for i in range(exact):
+        set_a(star_list)
+        for star in star_list:
+            star.run(1 / FPS / exact)
+    # 将背景图画上去
+    screen.fill((0, 0, 0))
+    max_v = 0
+    for star in star_list:
+        max_v = max(max_v, math.sqrt(star.vx**2+star.vy**2))
+        pygame.draw.circle(screen, (255, 0, 0), (int(star.x), int(star.y)), star.r)
+    exact = min(300,max(30,int(max_v)))*5
+    screen.blit(font.render("fps: " + str(clock.get_fps()), 1, THECOLORS["white"]), (0,0))
+    screen.blit(font.render("exact: " + str(exact), 1, THECOLORS["white"]), (0,15))
+
+    # 刷新画面
+    pygame.display.update()
+    time_passed = clock.tick(FPS)
 
 input()
