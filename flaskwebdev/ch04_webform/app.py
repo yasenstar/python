724fb36ec59801e0916795ed3e7abbf6be15b3cf
Yasen Zhao <xiaoqizhao@outlook.com>
2020-12-23 13:10:40 -0500

    update

---
+++ flaskwebdev/ch04_webform/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+
+app = Flask(__name__)
+
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
