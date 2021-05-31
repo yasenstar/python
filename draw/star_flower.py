f97f8f0ad35ba77a85210b92800965140bbe64ca
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2021-05-30 21:53:13 -0400

    Create star_flower.py

---
+++ draw/star_flower.py
@@ -0,0 +1,11 @@
+import turtle
+import time
+
+turtle.color("red", "yellow")
+turtle.begin_fill()
+for _ in range(50):
+    turtle.forward(200)
+    turtle.left(170)
+    turtle.end_fill()
+
+turtle.mainloop()
