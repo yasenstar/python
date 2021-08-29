395ea0034078406e6abbf368b2340cbba537b27d
Xiaoqi Zhao (Mac) <xiaoqizhao@outlook.com>
2021-08-29 13:14:50 -0400

    add learning Kaggle lesson 3

---
+++ Learn_in_Kaggle/conditionals.py
@@ -0,0 +1,15 @@
+def inspect(x):
+    if x == 0:
+        print(x, "is zero")
+    elif x > 0:
+        print(x, "is positive")
+    elif x < 0:
+        print(x, "is negative")
+    else:
+        print(x, "is unlike anything I've ever seen...")
+
+inspect(0)
+inspect(-15)
+inspect(100)
+inspect(True)
+inspect("yasen")
