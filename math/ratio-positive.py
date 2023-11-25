385d8ee93f88a0b9dde981ed82a713e55d67d830
Yasen <xiaoqizhao@outlook.com>
2023-11-24 19:20:26 -0500

    add plot for positive ratio

---
+++ math/ratio-positive.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# x = np.arange(1,11)
+# y = x * 2
+
+x = np.array([2,3,4,5,6])
+y = np.array([4,6,8,10,12])
+
+plt.title("test positive ratio")
+plt.xlabel("time")
+plt.ylabel("distance")
+plt.plot(x, y, color="red")
+plt.grid()
+plt.show()
