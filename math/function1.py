49616315b7e8ec4ea7788782e333448cf278dae5
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2023-11-26 18:54:21 -0500

    Update function1.py

--- math/function1.py
+++ math/function1.py
@@ -1,6 +1,18 @@
 # Challenge: a + 2ab + b = 22
 
+import numpy as np
+import matplotlib.pyplot as mpt
+
+x = []
+y = []
+
 for a in range(-100,100):
     for b in range(-100,100):
         if a + 2 * a * b + b == 22:
+            x.append(a)
+            y.append(b)
             print ("a=", a, " b=", b)
+
+mpt.grid()
+mpt.plot(x,y,color="red")
+mpt.show()
