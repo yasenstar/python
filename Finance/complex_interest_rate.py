82a3cdd5bc228ca4be3b115c70098a10a1650759
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2023-12-17 14:23:39 -0500

    Create complex_interest_rate.py

---
+++ Finance/complex_interest_rate.py
@@ -0,0 +1,19 @@
+# Purpose: use to calculate final principal after x years with compound interest
+# Variables:
+#   principal: 本金
+#   rate: interest rate 年存款利率
+#   n: 存款年数
+#   result: 结果
+
+principal = 100
+rate = 0.015
+n = 10
+result = principal
+
+for i in range(1,n+1):
+    result=result*(1+rate)
+    # print("After ", i, " years, principal is ", result)
+    print("after {} year, principal is {}".format(i, result))
+    
+print("*" * 50)
+print("final= ",result)
