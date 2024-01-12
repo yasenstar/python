4b285aa9598654bc290b54520ab58f96471c332b
Yasen <xiaoqizhao@outlook.com>
2024-01-11 21:25:05 -0500

    Update ratio-positive.py

--- math/ratio-positive.py
+++ math/ratio-positive.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 # y = x * 2
 
 x = np.array([2,3,4,5,6])
-y = np.array([4,6,8,10,12])
-
+# y = np.array([4,6,8,10,12])
+y=sin(x)
 plt.title("test positive ratio")
 plt.xlabel("time")
 plt.ylabel("distance")
