31c1844db2402f291a1d09210179133cd16007d0
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2024-04-21 10:54:08 -0400

    add new book GUI with Python

---
+++ GUI_Python/Create_GUI_with_Python.mm
@@ -0,0 +1,97 @@
+<map version="freeplane 1.11.5">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Create GUI with Python&#xa;(guizero library)" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1713710932564">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<hook NAME="MapStyle" background="#f9f9f8">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false" associatedTemplateLocation="template:/light_nord_template.mm"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_506805493" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#484747" BACKGROUND_COLOR="#efefef" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#8fbcbb" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_506805493" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#2e3440" WIDTH="3" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#ffffff" BACKGROUND_COLOR="#2e3440" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true">
+<font SIZE="11" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT">
+<icon BUILTIN="clock2"/>
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#eceff4" BACKGROUND_COLOR="#bf616a" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#bf616a"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_915433779" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_915433779"/>
+<font NAME="Ubuntu" SIZE="14"/>
+<edge COLOR="#bf616a"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#484747" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#eceff4" BACKGROUND_COLOR="#d08770" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#3b4252" BACKGROUND_COLOR="#ebcb8b">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#2e3440" BACKGROUND_COLOR="#a3be8c">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#2e3440" BACKGROUND_COLOR="#b48ead">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#e5e9f0" BACKGROUND_COLOR="#5e81ac">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BACKGROUND_COLOR="#81a1c1">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BACKGROUND_COLOR="#88c0d0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#8fbcbb">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BACKGROUND_COLOR="#d8dee9">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BACKGROUND_COLOR="#e5e9f0">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="CH01 Introduction to GUIs" POSITION="bottom_or_right" ID="ID_1433266949" CREATED="1713710941769" MODIFIED="1713710949628"/>
+<node TEXT="CH02 Wanted Poster" POSITION="bottom_or_right" ID="ID_1496937770" CREATED="1713710949832" MODIFIED="1713710955068"/>
+<node TEXT="CH03 Spy Name Chooser" POSITION="bottom_or_right" ID="ID_1259924064" CREATED="1713710955476" MODIFIED="1713710962191"/>
+<node TEXT="CH04 Meme Generator" POSITION="bottom_or_right" ID="ID_27606916" CREATED="1713710962517" MODIFIED="1713710968245"/>
+<node TEXT="CH05 World&apos;s Worst GUI" POSITION="bottom_or_right" ID="ID_1281317216" CREATED="1713710968405" MODIFIED="1713710976812"/>
+<node TEXT="CH06 Tic-Tac-Toe" POSITION="bottom_or_right" ID="ID_1070134055" CREATED="1713710976984" MODIFIED="1713710983862"/>
+<node TEXT="CH07 Destroy the Dots" POSITION="bottom_or_right" ID="ID_1566046552" CREATED="1713710984025" MODIFIED="1713710992202"/>
+<node TEXT="CH08 Flood It" POSITION="bottom_or_right" ID="ID_698084358" CREATED="1713710992374" MODIFIED="1713711001324"/>
+<node TEXT="CH09 Emoji Match" POSITION="bottom_or_right" ID="ID_1287415163" CREATED="1713711001486" MODIFIED="1713711005886"/>
+<node TEXT="CH10 Paint" POSITION="bottom_or_right" ID="ID_525310427" CREATED="1713711006044" MODIFIED="1713711009159"/>
+<node TEXT="CH11 Stop-Frame Animation" POSITION="bottom_or_right" ID="ID_1759909610" CREATED="1713711009314" MODIFIED="1713711015099"/>
+<node TEXT="Appendix A: Setting Up" POSITION="bottom_or_right" ID="ID_1526468538" CREATED="1713711015432" MODIFIED="1713711022300"/>
+<node TEXT="Appendix B: Getting Started with Python" POSITION="bottom_or_right" ID="ID_1618190413" CREATED="1713711022445" MODIFIED="1713711030913"/>
+<node TEXT="Appendix C: Widgets in guizero" POSITION="bottom_or_right" ID="ID_125872287" CREATED="1713711031062" MODIFIED="1713711043728"/>
+</node>
+</map>
---
+++ GUI_Python/README.md
@@ -0,0 +1,13 @@
+# Create Graphical User Interfaces with Python
+
+Author: Laura Sach, Marting O'Hanlon
+
+This repo aim to learn the book about GUI in Python with practicing!
+
+## Book Structure
+
+![book_content](img\book_cover.png)
+
+---
+
+[Email me](mailto:xiaoqizhao@outlook.com) for any questions or join the discussion board.
