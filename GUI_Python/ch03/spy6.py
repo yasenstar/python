bcda0f4be929b5d4c32ed6c47bcef619b8d84624
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2024-04-22 13:26:35 -0400

    additional 2 for ch03

---
+++ GUI_Python/ch03/spy6.py
@@ -0,0 +1,25 @@
+from guizero import App, Text, PushButton
+from random import choice
+
+first_names = ["Barbar", "Woody", "Tiberius", "Smokey", "Jennifer", "Rugy", "Yasen"]
+last_names = ["Spindleshands", "Mysterioso", "Dungeon", "Catseye", "Zhao", "Darkmeyer", "Flamingobreath"]
+
+def choose_name(a,b):
+    # print("Button is pressed!")
+    spy_name = choice(a) + " " + choice(b)
+    # print(spy_name)
+    name.value = spy_name
+
+app = App("TOP SECRET")
+
+title = Text(app, "Push the RED button to find out your spy name")
+
+button = PushButton(app, choose_name, args=[first_names,last_names], text="Tell me!")
+button.bg = "red"
+button.text_color = "white"
+button.text_size = 30
+
+name = Text(app, text="")
+name.text_size = 30
+
+app.display()
