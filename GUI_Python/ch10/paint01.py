6853a88e5e6b0287fdf03e71cbf25824e81dfae6
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2024-04-26 21:58:14 -0400

    chapter 10 part 1

---
+++ GUI_Python/ch10/paint01.py
@@ -0,0 +1,19 @@
+# Imports
+from guizero import App, Drawing
+
+# Functions
+def draw(event):
+    painting.oval(
+        event.x - 1, event.y - 1,
+        event.x + 1, event.y + 1,
+        color="black"
+    )
+
+# Apps
+app = App("Paint")
+
+painting = Drawing(app, width="fill", height="fill")
+
+painting.when_mouse_dragged = draw
+
+app.display()
---
+++ GUI_Python/ch10/paint02.py
@@ -0,0 +1,25 @@
+# Imports
+from guizero import App, Drawing
+
+# Functions
+def start(event):
+    painting.last_event = event
+
+def draw(event):
+    painting.line(
+        painting.last_event.x, painting.last_event.y,
+        event.x, event.y,
+        color="black",
+        width = 3
+    )
+    painting.last_event = event
+
+# Apps
+app = App("Paint")
+
+painting = Drawing(app, width="fill", height="fill")
+
+painting.when_left_button_pressed = start
+painting.when_mouse_dragged = draw
+
+app.display()
