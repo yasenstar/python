3904360289867619746cb274615f2d4846937ff1
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2024-11-12 19:45:22 +0800

    prepare manim

---
+++ emoji.py
@@ -0,0 +1,9 @@
+import emoji
+def text_to_emoji(text):
+    return emoji.emojize(text)
+
+input_text = input("enter emoji aliases: ")
+
+converted_text = text_to_emoji(input_text)
+
+print("result is: ", converted_text)
