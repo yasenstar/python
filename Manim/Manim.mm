015f06302d2707b2cc0ce8d29bc94a2a920a39a8
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2024-11-13 15:45:54 +0800

    update

---
+++ Manim/Manim.mm
@@ -0,0 +1,177 @@
+<map version="freeplane 1.11.5">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Manim" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="Freemind_Link_1513112588" CREATED="1153430895318" MODIFIED="1731412051886" LINK="https://www.manim.community/">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<hook NAME="MapStyle" background="#ffffff">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false" associatedTemplateLocation="template:/light_super_hero_template.mm"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_249848891" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#000000" BACKGROUND_COLOR="#fff024" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="3 pt">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_249848891" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#09387a" WIDTH="3" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#fff024" BACKGROUND_COLOR="#000000">
+<font BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT">
+<icon BUILTIN="clock2"/>
+<font SIZE="10" ITALIC="true"/>
+<edge COLOR="#000000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#000000" BACKGROUND_COLOR="#f5131f" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f5131f"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_254323574" STYLE="narrow_hexagon" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#c3131f">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#c3131f" TRANSPARENCY="255" DESTINATION="ID_254323574"/>
+<font SIZE="11" BOLD="true"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#000000" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt" BORDER_WIDTH="3.1 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#2c2b29" BORDER_DASH_LIKE_EDGE="true">
+<font NAME="Ubuntu" SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#ffffff" BACKGROUND_COLOR="#09387a" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR="#2c2b29">
+<font NAME="Ubuntu" SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#ffffff" BACKGROUND_COLOR="#126cb3" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#000000" BACKGROUND_COLOR="#d4e226" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#000000" BACKGROUND_COLOR="#fff024" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" BACKGROUND_COLOR="#fff024" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BACKGROUND_COLOR="#fff024" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BACKGROUND_COLOR="#fff024" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#fff024" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BORDER_COLOR="#f0f0f0">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11" BORDER_COLOR="#f0f0f0">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="Manim Installation" POSITION="bottom_or_right" ID="ID_358303331" CREATED="1731412195668" MODIFIED="1731412231379">
+<node TEXT="Windows OS" ID="ID_554992987" CREATED="1731412231383" MODIFIED="1731412234251"/>
+</node>
+<node TEXT="Tutorials &amp; Guides" POSITION="bottom_or_right" ID="ID_1759134914" CREATED="1731412235228" MODIFIED="1731412247186">
+<node TEXT="Tutorials" ID="ID_938155600" CREATED="1731412247192" MODIFIED="1731412253573">
+<node TEXT="Quickstart" ID="ID_1252296146" CREATED="1731412253581" MODIFIED="1731412257549">
+<node TEXT="New Project, Animating a Circle" ID="ID_658640489" CREATED="1731412279490" MODIFIED="1731412292284"/>
+<node TEXT="Transformaing a Square into a Circle" ID="ID_1740838145" CREATED="1731412298593" MODIFIED="1731412306214"/>
+<node TEXT="Positioning MobjectS" ID="ID_482986914" CREATED="1731412367011" MODIFIED="1731412380230"/>
+<node TEXT="Using .animate syntax to animate methods" ID="ID_1657303519" CREATED="1731412412857" MODIFIED="1731412423190"/>
+<node TEXT="Transform vs. ReplacementTransform" ID="ID_1386461022" CREATED="1731412438562" MODIFIED="1731412451916"/>
+</node>
+<node TEXT="Homepage&apos;s Examples" ID="ID_1215655236" CREATED="1731412707826" MODIFIED="1731412717486">
+<node TEXT="Example: ContinousMotion" ID="ID_1335818196" CREATED="1731412741643" MODIFIED="1731412765639"/>
+<node TEXT="Example: OpeningManim" ID="ID_573002913" CREATED="1731412741810" MODIFIED="1731412803481"/>
+<node TEXT="Example: SquareToCircle" ID="ID_602626986" CREATED="1731412717491" MODIFIED="1731412727495"/>
+<node TEXT="Example: UnionExample" ID="ID_262791385" CREATED="1731412745498" MODIFIED="1731412824950"/>
+<node TEXT="Example: WarpSquare" ID="ID_1067358148" CREATED="1731412746470" MODIFIED="1731412751328"/>
+</node>
+<node TEXT="Manim&apos;s Output Settings" ID="ID_1579266090" CREATED="1731412471779" MODIFIED="1731412478969">
+<node TEXT="Manim Output Folders" ID="ID_1041352639" CREATED="1731413062835" MODIFIED="1731413068805"/>
+<node TEXT="Sections" ID="ID_1697259484" CREATED="1731413069262" MODIFIED="1731413074382"/>
+<node TEXT="Some Command Line Flags" ID="ID_367989811" CREATED="1731413083277" MODIFIED="1731413088421"/>
+</node>
+<node TEXT="Manim&apos;s Buidling Blocks" ID="ID_41258804" CREATED="1731413090500" MODIFIED="1731413095063">
+<node TEXT="Mobjects" ID="ID_1256892739" CREATED="1731413108956" MODIFIED="1731413112163">
+<node TEXT="Creating and Displaying mobjects" ID="ID_1395858482" CREATED="1731413120243" MODIFIED="1731413148174"/>
+<node TEXT="Placing mobjects" ID="ID_924292030" CREATED="1731413128280" MODIFIED="1731413136715"/>
+<node TEXT="Styling mobjects" ID="ID_680763027" CREATED="1731414376583" MODIFIED="1731414380952"/>
+<node TEXT="Mobject on-screen order" ID="ID_1449211440" CREATED="1731414386672" MODIFIED="1731414391907"/>
+</node>
+<node TEXT="Animations" ID="ID_1621498316" CREATED="1731414396625" MODIFIED="1731414398998">
+<node TEXT="Animating methods" ID="ID_1069110668" CREATED="1731414406518" MODIFIED="1731414412844"/>
+<node TEXT="Animating run time" ID="ID_1220379991" CREATED="1731414416920" MODIFIED="1731414421090"/>
+<node TEXT="Creating a custom animation" ID="ID_19909403" CREATED="1731414425287" MODIFIED="1731414431592"/>
+<node TEXT="Using coordinates of a mobject" ID="ID_58778808" CREATED="1731414437927" MODIFIED="1731414446690"/>
+<node TEXT="Transforming mobjects into other mobjects" ID="ID_205777661" CREATED="1731414457033" MODIFIED="1731414474814"/>
+</node>
+<node TEXT="Scenes" ID="ID_908281644" CREATED="1731414481353" MODIFIED="1731414500296"/>
+</node>
+</node>
+<node TEXT="Thematic Guides" ID="ID_1918798362" CREATED="1731414501109" MODIFIED="1731414505354">
+<node TEXT="Configuration" ID="ID_515279388" CREATED="1731414505363" MODIFIED="1731414614430">
+<node TEXT="Command-line arguments" ID="ID_612662919" CREATED="1731414659913" MODIFIED="1731414666046">
+<node TEXT="Advanced examples" ID="ID_1260834701" CREATED="1731416118050" MODIFIED="1731416122678"/>
+<node TEXT="A list of all CLI flags" ID="ID_714064595" CREATED="1731416126767" MODIFIED="1731416131925"/>
+</node>
+<node TEXT="The ManimConfig class" ID="ID_217433903" CREATED="1731414689446" MODIFIED="1731414696652"/>
+<node TEXT="The config files" ID="ID_1712805652" CREATED="1731414696819" MODIFIED="1731414700692">
+<node TEXT="The user config file" ID="ID_1508570480" CREATED="1731418139124" MODIFIED="1731418143892"/>
+<node TEXT="Cascading config files" ID="ID_1438346715" CREATED="1731418146512" MODIFIED="1731418151602"/>
+</node>
+<node TEXT="Order of operations" ID="ID_1687146465" CREATED="1731414700875" MODIFIED="1731414704489"/>
+<node TEXT="A list of all config options" ID="ID_728405859" CREATED="1731414704658" MODIFIED="1731414709687"/>
+<node TEXT="Accessing CLI command options" ID="ID_1232583923" CREATED="1731414709859" MODIFIED="1731414716100"/>
+</node>
+<node TEXT="A deep dive into Manim&apos;s internals" ID="ID_1696588554" CREATED="1731414614954" MODIFIED="1731414623113">
+<node TEXT="Introduction" ID="ID_1246114774" CREATED="1731416051193" MODIFIED="1731416055163"/>
+<node TEXT="Overview" ID="ID_1819656604" CREATED="1731416055328" MODIFIED="1731416056745"/>
+<node TEXT="Preliminaries" ID="ID_902556039" CREATED="1731416056894" MODIFIED="1731416062265">
+<node TEXT="Importing the library" ID="ID_513852156" CREATED="1731418371922" MODIFIED="1731418377069"/>
+<node TEXT="Scene instantiation and rendering" ID="ID_1602532168" CREATED="1731418380155" MODIFIED="1731418386798"/>
+</node>
+<node TEXT="Mobject Initialization" ID="ID_896497173" CREATED="1731416062424" MODIFIED="1731416067727">
+<node TEXT="What even is a Mobject?" ID="ID_862038292" CREATED="1731418393538" MODIFIED="1731418398813"/>
+<node TEXT="...and what are VMobjects" ID="ID_1903781900" CREATED="1731418402470" MODIFIED="1731418408162"/>
+<node TEXT="Squares and Circles: back to our Toy Example" ID="ID_1252174351" CREATED="1731418413512" MODIFIED="1731418421767"/>
+<node TEXT="Adding Mobjects to the Scene" ID="ID_152907864" CREATED="1731418425602" MODIFIED="1731418432139"/>
+</node>
+<node TEXT="Animations and the Render Loop" ID="ID_59951791" CREATED="1731416067888" MODIFIED="1731416076626">
+<node TEXT="Initializing animations" ID="ID_1630160512" CREATED="1731418436798" MODIFIED="1731418443856"/>
+<node TEXT="The play call: preparing to enter Mainm&apos;s render loop" ID="ID_440875323" CREATED="1731418446125" MODIFIED="1731418457671"/>
+<node TEXT="The render loop (for real this time)" ID="ID_874357964" CREATED="1731418462437" MODIFIED="1731418468684"/>
+<node TEXT="Completing the render loop" ID="ID_1645699632" CREATED="1731418473790" MODIFIED="1731418478406"/>
+</node>
+</node>
+<node TEXT="Rendering Text and Formulas" ID="ID_876839294" CREATED="1731414623288" MODIFIED="1731414629497">
+<node TEXT="Text Without LaTeX" ID="ID_706641445" CREATED="1731416079009" MODIFIED="1731416085225">
+<node TEXT="Working with Text" ID="ID_1696881758" CREATED="1731418494258" MODIFIED="1731418503341">
+<node TEXT="Using Fonts" ID="ID_1966523630" CREATED="1731418503345" MODIFIED="1731418505729"/>
+<node TEXT="Setting Slant and Weight" ID="ID_485848959" CREATED="1731418509830" MODIFIED="1731418514462"/>
+<node TEXT="Using Colors" ID="ID_1359173194" CREATED="1731418522139" MODIFIED="1731418525673"/>
+<node TEXT="Using Gradients" ID="ID_1068338732" CREATED="1731418535533" MODIFIED="1731418538931"/>
+</node>
+</node>
+<node TEXT="Text With LaTex" ID="ID_1162723049" CREATED="1731416085405" MODIFIED="1731416094108"/>
+</node>
+<node TEXT="Adding Voiceovers to Videos" ID="ID_1035570971" CREATED="1731414629697" MODIFIED="1731414635725">
+<node TEXT="Basic Usage" ID="ID_755153236" CREATED="1731416095359" MODIFIED="1731416098973"/>
+</node>
+</node>
+</node>
+</node>
+</map>
