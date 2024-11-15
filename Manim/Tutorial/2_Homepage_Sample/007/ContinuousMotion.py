d5e68dec9715af7503e570366a0402125a16dae7
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2024-11-15 23:24:38 +0800

    add 007

---
+++ Manim/Tutorial/2_Homepage_Sample/007/ContinuousMotion.py
@@ -0,0 +1,9 @@
+from manim import *
+
+class ContinuousMotion(Scene):
+    def construct(self):
+        func = lambda pos: np.sin(pos[0] / 2) * UR + np.cos(pos[1] / 2) * LEFT
+        stream_lines = StreamLines(func, stroke_width=4, max_anchors_per_line=30)
+        self.add(stream_lines)
+        stream_lines.start_animation(warm_up=False, flow_speed=0.5)
+        self.wait(stream_lines.virtual_time / stream_lines.flow_speed)
