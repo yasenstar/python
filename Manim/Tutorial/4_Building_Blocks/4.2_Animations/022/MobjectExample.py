089e482d387a91959caa97ce36fd591b8bbb9bf3
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2024-11-20 19:24:19 +0800

    Update MobjectExample.py

--- Manim/Tutorial/4_Building_Blocks/4.2_Animations/022/MobjectExample.py
+++ Manim/Tutorial/4_Building_Blocks/4.2_Animations/022/MobjectExample.py
@@ -1,2 +1,10 @@
 from manim import *
 
+class MobjectExample(Scene):
+    def construct(self):
+        p1 = [-1, -1, 0]
+        p2 = [1, -1, 0]
+        p3 = [1, 1, 0]
+        p4 = [-1, 1, 0]
+
+        
