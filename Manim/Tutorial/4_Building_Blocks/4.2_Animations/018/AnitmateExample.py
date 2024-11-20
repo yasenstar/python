a02f03a56da88e27fa08726b53e6b35b78a49a54
Xiaoqi Zhao <xiaoqizhao@outlook.com>
2024-11-20 09:51:05 +0800

    add 018

---
+++ Manim/Tutorial/4_Building_Blocks/4.2_Animations/018/AnitmateExample.py
@@ -0,0 +1,21 @@
+from manim import *
+
+class AnimateExample(Scene):
+    def construct(self):
+        square = Square().set_fill(RED, opacity=1.0)
+        self.add(square)
+
+        self.play(square.animate.set_fill(WHITE))
+        self.wait(1)
+
+        self.play(square.animate.shift(UP).rotate(PI/3))
+        self.wait(1)
+
+        self.add(square.set_fill(BLUE))
+        self.wait(1)
+
+        self.play(square.animate.shift(UP).rotate(PI/3))
+        self.wait(1)
+
+        self.play(square.animate.set_fill(YELLOW))
+        self.wait(1)
