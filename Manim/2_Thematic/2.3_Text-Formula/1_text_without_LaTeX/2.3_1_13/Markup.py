4975fb6a401c8ad0a3750660456fdfeb6d158572
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2024-12-07 23:09:08 +0800

    add LaTeX overview

---
+++ Manim/2_Thematic/2.3_Text-Formula/1_text_without_LaTeX/2.3_1_13/Markup.py
@@ -0,0 +1,10 @@
+from manim import *
+
+class MarkupTest(Scene):
+    def construct(self):
+        text = MarkupText(
+            f'<span underline="double" underline_color="green">double green underline</span> in red text<span fgcolor="{YELLOW}"> except this</span>',
+            color=RED,
+            font_size=34
+        )
+        self.add(text)
---
+++ Manim/2_Thematic/2.3_Text-Formula/2_text_with_LaTeX/2.3_2_01/HelloLaTeX.py
@@ -0,0 +1,6 @@
+from manim import *
+
+class HelloLaTeX(Scene):
+    def construct(self):
+        tex = Tex(r"\LaTeX", font_size=144)
+        self.add(tex)
