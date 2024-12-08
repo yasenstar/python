0e9cc8e4ed12e69653d8472906f7f8f32516d45b
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2024-12-08 11:00:02 +0800

    add MathLaX sample

---
+++ Manim/2_Thematic/2.3_Text-Formula/2_text_with_LaTeX/2.3_2_02/MathTeX.py
@@ -0,0 +1,7 @@
+from manim import *
+
+class MathTeXDemo(Scene):
+    def construct(self):
+        rtarrow0 = MathTex(r"\xrightarrow{x^6y^8}", font_size=96)
+        rtarrow1 = Tex(r"$\xrightarrow{x^6y^8}$", font_size=96)
+        self.add(VGroup(rtarrow0, rtarrow1).arrange(DOWN))
