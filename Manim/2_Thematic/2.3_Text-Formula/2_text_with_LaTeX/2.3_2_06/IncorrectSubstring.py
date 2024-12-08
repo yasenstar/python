3ce0755c6307dacaed5de27d47b3c8b33f85ed92
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2024-12-08 16:25:00 +0800

    add 2.3_2_06

---
+++ Manim/2_Thematic/2.3_Text-Formula/2_text_with_LaTeX/2.3_2_06/IncorrectSubstring.py
@@ -0,0 +1,14 @@
+from manim import *
+
+class IncorrectLaTeXSubstringColoring(Scene):
+    def construct(self):
+        equation = MathTex(
+            r"e^x = \
+            x^0 + x^1 + \frac{1}{2} x^2 + \
+            \frac{1}{6}x^3 + ... + \cdots +\
+            \frac{1}{n!} x^n + \cdots",
+            r"\frac{x^2+y^3}{z^(3+y)}"
+        )
+        equation.set_color_by_tex('x', RED)
+        equation[1].set_color(PURPLE)
+        self.add(equation)
