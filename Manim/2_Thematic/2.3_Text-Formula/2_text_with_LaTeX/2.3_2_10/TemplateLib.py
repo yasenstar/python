fd38151b3fc5d251220fccec545a76a76a7ff161
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2024-12-10 08:09:09 +0800

    add

---
+++ Manim/2_Thematic/2.3_Text-Formula/2_text_with_LaTeX/2.3_2_10/TemplateLib.py
@@ -0,0 +1,10 @@
+from manim import *
+
+class LaTeXTemplateLibrary(Scene):
+    def construct(self):
+        tex = Tex(
+            'Hello 你好 \\LaTeX',
+            tex_template=TexTemplateLibrary.ctex,
+            font_size = 144
+        )
+        self.add(tex)
