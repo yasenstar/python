83a0742266f5514fa74defed31467b67398a697b
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2024-12-10 09:54:43 +0800

    troubleshooting ctex

--- Manim/2_Thematic/2.3_Text-Formula/2_text_with_LaTeX/2.3_2_10/TemplateLib.py
+++ Manim/2_Thematic/2.3_Text-Formula/2_text_with_LaTeX/2.3_2_10/TemplateLib.py
@@ -1,10 +1,18 @@
 from manim import *
 
-class LaTeXTemplateLibrary(Scene):
+# class LaTeXTemplateLibrary(Scene):
+#     def construct(self):
+        # tex = Tex(
+        #     r"{你好}",
+        #     tex_template=TexTemplateLibrary.ctex,
+        #     font_size = 96
+        #     # font = "simhei"
+        # )
+        # self.add(tex)
+class demo(Scene):
     def construct(self):
-        tex = Tex(
-            'Hello 你好 \\LaTeX',
-            tex_template=TexTemplateLibrary.ctex,
-            font_size = 144
-        )
-        self.add(tex)
+        t1 = MathTex(r"\text{你好}", tex_template=TexTemplateLibrary.ctex)
+        self.play(Write(t1))
+        t2 = Tex(r"你好", tex_template=TexTemplateLibrary.ctex)
+        self.play(Write(t2).set_color(RED).next_to(t1,DOWN))
+        self.wait()
