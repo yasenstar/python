88748244a267af7f169d70a77a8d0cfb59fa81a9
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2025-03-28 21:43:52 +0800

    initial new book learning

---
+++ Sci_Prog/Scientific_Programming_with_Python.md
@@ -0,0 +1,111 @@
+ Scientific Programming
+with Python
+
+# 0. Opening and Preface
+
+# 1. Getting Started with Python
+
+## 1.1 The First Example: Hello, World!
+
+## 1.2 Different Ways to Use Python
+
+# 2. Computing with Formulas
+
+## 2.1 Programming Simple Mathematics
+
+## 2.2 Variables and Variable Types
+
+## 2.3 Formatting Text Output
+
+## 2.4 Importing Modules
+
+## 2.5 Pitfalls When Programming Mathematics
+
+# 3. Loops and Lists
+
+## 3.1 Loops for Automating Repetitive Tasks
+
+## 3.2 Boolean Expressions
+
+## 3.3 Using Lists to Store Sequences of Data
+
+## 3.4 Iterating Over a List with a FOR Loop
+
+## 3.5 Nested Lists and List Slicing
+
+## 3.6 Tuples
+
+# 4. Functions and Branching
+
+## 4.1 Programming with Functions
+
+## 4.2 Function Arguments and Local Variables
+
+## 4.3 Default Arguments and Doc Strings
+
+## 4.4 If-Tests for Branching the Program Flow
+
+## 4.5 Functions as Arguments to Functions
+
+## 4.6 Solving Equations with Python Functions
+
+## 4.7 Writing Test Functions to Verify our Programs
+
+# 5. User Input and Error Handling
+
+## 5.1 Reading User Input Data
+ 
+5.2 Flexible User Input witheval  andexec
+
+
+## 5.3 Reading Data from Files
+
+## 5.4 Writing Data to Files
+
+## 5.5 Handling Errors in Programs
+
+## 5.6 Making Modules
+
+# 6. Arrays and Plotting
+
+## 6.1 NumPy and Array Computing
+
+## 6.2 Plotting Curves with Matplotlib
+
+## 6.3 Plotting Discontinuous and Piecewise-Defined Functions
+
+## 6.4 Making a Movie of a Plot
+
+## 6.5 More Useful Array Operations
+
+# 7. Dictionaries and Strings
+
+## 7.1 Dictionaries
+
+## 7.2 Example: A Dictionary for Polynomials
+
+## 7.3 Example: Reading File Data to a Dictionary
+
+## 7.4 String Manipulation
+
+# 8. Classes
+
+## 8.1 Basics of Classes
+
+## 8.2 Protected Class Attributes
+
+## 8.3 Special Methods
+
+## 8.4 Example: Automatic Differentiation of Functions
+
+## 8.5 Test Functions for Classes
+
+## 8.6 Example: A Polynomial Class
+
+# 9. Object-Oriented Porgramming
+
+## 9.1 Class Hierarchies and Inheritance
+
+## 9.2 Example: Classes for Numerical Differentiation
+
+## 9.3 Example: Classes for Numerical Integration
---
+++ Sci_Prog/Scientific_Programming_with_Python.mm
@@ -0,0 +1,167 @@
+<map version="freeplane 1.12.1">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Scientific Programming&#xa;with Python" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1743167852458">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<hook NAME="MapStyle" background="#fdf6e3ff" zoom="0.826">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false" show_icons="BESIDE_NODES" associatedTemplateLocation="template:/light_solarized_template.mm" show_tags="UNDER_NODES"/>
+    <tags category_separator="::"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_1704360557" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#002b36" BACKGROUND_COLOR="#fdf6e3" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="3 pt">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1704360557" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#93a1a1" WIDTH="3" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT">
+<icon BUILTIN="clock2"/>
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#fdf6e3" BACKGROUND_COLOR="#d33682" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#d33682"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_1083719472" COLOR="#ffffff" BACKGROUND_COLOR="#bf616a" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_1083719472"/>
+<font SIZE="12" BOLD="true"/>
+<edge COLOR="#bf616a"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#002b36" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt" BORDER_WIDTH="3.1 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#2c2b29" BORDER_DASH_LIKE_EDGE="true">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#fdf6e3" BACKGROUND_COLOR="#073642" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR="#2c2b29">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#fdf6e3" BACKGROUND_COLOR="#586e75" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#fdf6e3" BACKGROUND_COLOR="#657b83" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#fdf6e3" BACKGROUND_COLOR="#839496" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#fdf6e3" BACKGROUND_COLOR="#93a1a1" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BACKGROUND_COLOR="#eee8d5" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BACKGROUND_COLOR="#eedfcc" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#fdf6e3" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BORDER_COLOR="#f0f0f0">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11" BORDER_COLOR="#f0f0f0" BACKGROUND_COLOR="#fdf6e3">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="0. Opening and Preface" POSITION="bottom_or_right" ID="ID_1293353710" CREATED="1743168665305" MODIFIED="1743168671787"/>
+<node TEXT="1. Getting Started with Python" POSITION="bottom_or_right" ID="ID_143010520" CREATED="1743168594147" MODIFIED="1743168601866">
+<node TEXT="1.1 The First Example: Hello, World!" ID="ID_1661772416" CREATED="1743168679431" MODIFIED="1743168691067"/>
+<node TEXT="1.2 Different Ways to Use Python" ID="ID_1727367702" CREATED="1743168691216" MODIFIED="1743168702365"/>
+</node>
+<node TEXT="2. Computing with Formulas" POSITION="bottom_or_right" ID="ID_564065981" CREATED="1743168602129" MODIFIED="1743168610783">
+<node TEXT="2.1 Programming Simple Mathematics" ID="ID_773349951" CREATED="1743168741090" MODIFIED="1743168748965"/>
+<node TEXT="2.2 Variables and Variable Types" ID="ID_690791292" CREATED="1743168749471" MODIFIED="1743168756689"/>
+<node TEXT="2.3 Formatting Text Output" ID="ID_93569459" CREATED="1743168756857" MODIFIED="1743168763010"/>
+<node TEXT="2.4 Importing Modules" ID="ID_730556564" CREATED="1743168763167" MODIFIED="1743168770057"/>
+<node TEXT="2.5 Pitfalls When Programming Mathematics" ID="ID_1404843300" CREATED="1743168770222" MODIFIED="1743168781213"/>
+</node>
+<node TEXT="3. Loops and Lists" POSITION="bottom_or_right" ID="ID_1710903860" CREATED="1743168610977" MODIFIED="1743168619066">
+<node TEXT="3.1 Loops for Automating Repetitive Tasks" ID="ID_1233321647" CREATED="1743168785687" MODIFIED="1743168800763"/>
+<node TEXT="3.2 Boolean Expressions" ID="ID_27666289" CREATED="1743168800937" MODIFIED="1743168807305"/>
+<node TEXT="3.3 Using Lists to Store Sequences of Data" ID="ID_824240828" CREATED="1743168807478" MODIFIED="1743168817484"/>
+<node TEXT="3.4 Iterating Over a List with a FOR Loop" ID="ID_1864018603" CREATED="1743168817639" MODIFIED="1743168831610"/>
+<node TEXT="3.5 Nested Lists and List Slicing" ID="ID_1769912890" CREATED="1743168831759" MODIFIED="1743168843474"/>
+<node TEXT="3.6 Tuples" ID="ID_1091646665" CREATED="1743168843633" MODIFIED="1743168846423"/>
+</node>
+<node TEXT="4. Functions and Branching" POSITION="bottom_or_right" ID="ID_72079046" CREATED="1743168623989" MODIFIED="1743168630271">
+<node TEXT="4.1 Programming with Functions" ID="ID_483775829" CREATED="1743168847665" MODIFIED="1743168856363"/>
+<node TEXT="4.2 Function Arguments and Local Variables" ID="ID_101900104" CREATED="1743168856531" MODIFIED="1743168865899"/>
+<node TEXT="4.3 Default Arguments and Doc Strings" ID="ID_1161032010" CREATED="1743168866048" MODIFIED="1743168874018"/>
+<node TEXT="4.4 If-Tests for Branching the Program Flow" ID="ID_1399969090" CREATED="1743168874176" MODIFIED="1743168893617"/>
+<node TEXT="4.5 Functions as Arguments to Functions" ID="ID_92521020" CREATED="1743168893781" MODIFIED="1743168907569"/>
+<node TEXT="4.6 Solving Equations with Python Functions" ID="ID_827211987" CREATED="1743168907739" MODIFIED="1743168920793"/>
+<node TEXT="4.7 Writing Test Functions to Verify our Programs" ID="ID_246114758" CREATED="1743168920959" MODIFIED="1743168935724"/>
+</node>
+<node TEXT="5. User Input and Error Handling" POSITION="bottom_or_right" ID="ID_230809007" CREATED="1743168630459" MODIFIED="1743168636941">
+<node TEXT="5.1 Reading User Input Data" ID="ID_421945873" CREATED="1743168946815" MODIFIED="1743168956044"/>
+<node ID="ID_475463209" CREATED="1743168956192" MODIFIED="1743168972927"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      5.2 Flexible User Input with <span style="font-weight: bold;">eval</span><span style="font-weight: normal;">&#xa0;&#xa0;and </span><span style="font-weight: bold;">exec</span>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="5.3 Reading Data from Files" ID="ID_170133253" CREATED="1743168974552" MODIFIED="1743168982940"/>
+<node TEXT="5.4 Writing Data to Files" ID="ID_1593505266" CREATED="1743168983135" MODIFIED="1743168991105"/>
+<node TEXT="5.5 Handling Errors in Programs" ID="ID_443893628" CREATED="1743168991254" MODIFIED="1743169057790"/>
+<node TEXT="5.6 Making Modules" ID="ID_505472990" CREATED="1743169057945" MODIFIED="1743169062042"/>
+</node>
+<node TEXT="6. Arrays and Plotting" POSITION="bottom_or_right" ID="ID_1852317364" CREATED="1743168637097" MODIFIED="1743168641908">
+<node TEXT="6.1 NumPy and Array Computing" ID="ID_941164891" CREATED="1743169063998" MODIFIED="1743169078558"/>
+<node TEXT="6.2 Plotting Curves with Matplotlib" ID="ID_1696490335" CREATED="1743169074558" MODIFIED="1743169087287"/>
+<node TEXT="6.3 Plotting Discontinuous and Piecewise-Defined Functions" ID="ID_1039709397" CREATED="1743169087427" MODIFIED="1743169101726"/>
+<node TEXT="6.4 Making a Movie of a Plot" ID="ID_1830996185" CREATED="1743169101882" MODIFIED="1743169110309"/>
+<node TEXT="6.5 More Useful Array Operations" ID="ID_1172455462" CREATED="1743169110467" MODIFIED="1743169121070"/>
+</node>
+<node TEXT="7. Dictionaries and Strings" POSITION="bottom_or_right" ID="ID_1885413781" CREATED="1743168645248" MODIFIED="1743168651237">
+<node TEXT="7.1 Dictionaries" ID="ID_1782932541" CREATED="1743169123811" MODIFIED="1743169128878"/>
+<node TEXT="7.2 Example: A Dictionary for Polynomials" ID="ID_708546893" CREATED="1743169129051" MODIFIED="1743169140966"/>
+<node TEXT="7.3 Example: Reading File Data to a Dictionary" ID="ID_1604687300" CREATED="1743169141131" MODIFIED="1743169151801"/>
+<node TEXT="7.4 String Manipulation" ID="ID_42947235" CREATED="1743169151956" MODIFIED="1743169156774"/>
+</node>
+<node TEXT="8. Classes" POSITION="bottom_or_right" ID="ID_1934824475" CREATED="1743168651416" MODIFIED="1743168654611">
+<node TEXT="8.1 Basics of Classes" ID="ID_723547382" CREATED="1743169158074" MODIFIED="1743169166367"/>
+<node TEXT="8.2 Protected Class Attributes" ID="ID_452197333" CREATED="1743169166522" MODIFIED="1743169174341"/>
+<node TEXT="8.3 Special Methods" ID="ID_550322351" CREATED="1743169174498" MODIFIED="1743169178186"/>
+<node TEXT="8.4 Example: Automatic Differentiation of Functions" ID="ID_1005465663" CREATED="1743169178346" MODIFIED="1743169191567"/>
+<node TEXT="8.5 Test Functions for Classes" ID="ID_1463108285" CREATED="1743169191722" MODIFIED="1743169197256"/>
+<node TEXT="8.6 Example: A Polynomial Class" ID="ID_1588536663" CREATED="1743169197666" MODIFIED="1743169204654"/>
+</node>
+<node TEXT="9. Object-Oriented Porgramming" POSITION="bottom_or_right" ID="ID_1092068585" CREATED="1743168654777" MODIFIED="1743168663142">
+<node TEXT="9.1 Class Hierarchies and Inheritance" ID="ID_1252167424" CREATED="1743169207649" MODIFIED="1743169218064"/>
+<node TEXT="9.2 Example: Classes for Numerical Differentiation" ID="ID_1364377189" CREATED="1743169218211" MODIFIED="1743169230278"/>
+<node TEXT="9.3 Example: Classes for Numerical Integration" ID="ID_613514223" CREATED="1743169230426" MODIFIED="1743169241167"/>
+</node>
+</node>
+</map>
