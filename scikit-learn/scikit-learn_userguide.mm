0fc5a07a7986c82426883be1e29acb35a9d21e22
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2026-02-20 18:04:55 +0800

    initial scikit-learn

---
+++ scikit-learn/scikit-learn_userguide.mm
@@ -0,0 +1,133 @@
+<map version="freeplane 1.12.1">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="scikit-learn&#xa;(user guide)" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1771579253296" VGAP_QUANTITY="3 pt"><hook NAME="MapStyle" background="#d6e8e8ff" zoom="0.826">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false" show_icons="BESIDE_NODES" associatedTemplateLocation="template:/light_sky_element_template.mm" show_tags="UNDER_NODES"/>
+    <tags category_separator="::"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_4172522" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#051552" BACKGROUND_COLOR="#5cd5e8" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.7 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#1164b0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="3 pt">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_4172522" STARTINCLINATION="116.25 pt;0 pt;" ENDINCLINATION="116.25 pt;28.5 pt;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#051552" WIDTH="2" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#fff024" BACKGROUND_COLOR="#000000"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT">
+<icon BUILTIN="clock2"/>
+<font SIZE="10" ITALIC="true"/>
+<edge COLOR="#000000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#ffffff" BACKGROUND_COLOR="#cc7212" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#1164b0"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_1823054225" COLOR="#fff024" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#9d5e19">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#9d5e19" TRANSPARENCY="255" DESTINATION="ID_1823054225"/>
+<font SIZE="11" BOLD="true"/>
+<edge COLOR="#9d5e19"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#053d8b" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#2c2b29" BORDER_DASH_LIKE_EDGE="true">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#ffffff" BACKGROUND_COLOR="#1164b0" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR="#2c2b29">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#ffffff" BACKGROUND_COLOR="#298bc8" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#ffffff" BACKGROUND_COLOR="#3fb7db" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#051552" BACKGROUND_COLOR="#5cd5e8" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BORDER_COLOR="#f0f0f0">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11" BORDER_COLOR="#f0f0f0">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<node TEXT="01. Supervised Learning" POSITION="bottom_or_right" ID="ID_1726377366" CREATED="1771579294205" MODIFIED="1771579303902">
+<node TEXT="01.01 Linear Models" ID="ID_1085023519" CREATED="1771579454613" MODIFIED="1771579846004">
+<arrowlink DESTINATION="ID_889383436"/>
+<node TEXT="01.01.01 Ordinary Least Squares" ID="ID_510296935" CREATED="1771579563923" MODIFIED="1771579972762">
+<arrowlink DESTINATION="ID_1453621060"/>
+<node TEXT="01.01.01.01 Non-Negative Least Squares" ID="ID_1484523013" CREATED="1771579573219" MODIFIED="1771579609662"/>
+<node TEXT="01.01.01.02 Ordinary Least Squares Complexity" ID="ID_484024687" CREATED="1771579609860" MODIFIED="1771579629350"/>
+</node>
+<node TEXT="01.01.11 Logistic Regression" ID="ID_889383436" CREATED="1771579830283" MODIFIED="1771579837340"/>
+</node>
+<node TEXT="01.02 Linear and Quadratic Discriminant Analysis" ID="ID_562031506" CREATED="1771579467076" MODIFIED="1771579480502"/>
+<node TEXT="01.03 Kernel Ridge Regression" ID="ID_1924233243" CREATED="1771579480756" MODIFIED="1771579494118"/>
+<node TEXT="01.04 Support Vector Machines" ID="ID_1329266918" CREATED="1771579494292" MODIFIED="1771579501319"/>
+<node TEXT="01.05 Stochastic Gradient Descent" ID="ID_246639423" CREATED="1771579501484" MODIFIED="1771579510679"/>
+<node TEXT="01.06 Nearest Neighbors" ID="ID_255488266" CREATED="1771579510844" MODIFIED="1771579518302"/>
+<node TEXT="01.07 Gaussian Processes" ID="ID_1427447415" CREATED="1771579518533" MODIFIED="1771579524294"/>
+<node TEXT="01.08 Cross Decomposition" ID="ID_1909604391" CREATED="1771579524860" MODIFIED="1771579534054"/>
+</node>
+<node TEXT="02. Unsupervised Learning" POSITION="bottom_or_right" ID="ID_162058401" CREATED="1771579304084" MODIFIED="1771579314312"/>
+<node TEXT="03. Model Selection and Evaluation" POSITION="bottom_or_right" ID="ID_1950231085" CREATED="1771579314700" MODIFIED="1771579324751"/>
+<node TEXT="04. Metadata Routing" POSITION="bottom_or_right" ID="ID_375450798" CREATED="1771579325004" MODIFIED="1771579330872"/>
+<node TEXT="05. Inspection" POSITION="bottom_or_right" ID="ID_186482125" CREATED="1771579331045" MODIFIED="1771579337367"/>
+<node TEXT="06. Visualizations" POSITION="bottom_or_right" ID="ID_1722288240" CREATED="1771579337549" MODIFIED="1771579342657"/>
+<node TEXT="07. Dataset Transformations" POSITION="bottom_or_right" ID="ID_591875520" CREATED="1771579342820" MODIFIED="1771579349296"/>
+<node TEXT="08. Dataset Loading Utilities" POSITION="bottom_or_right" ID="ID_1657764729" CREATED="1771579349469" MODIFIED="1771579356798"/>
+<node TEXT="09. Computing with scikit-learn" POSITION="bottom_or_right" ID="ID_297070828" CREATED="1771579356988" MODIFIED="1771579367071"/>
+<node TEXT="10. Model Persistence" POSITION="bottom_or_right" ID="ID_374955857" CREATED="1771579367669" MODIFIED="1771579373711"/>
+<node TEXT="11. Common Pitfalls and Recommended Practices" POSITION="bottom_or_right" ID="ID_47716399" CREATED="1771579373884" MODIFIED="1771579386622"/>
+<node TEXT="12. Dispatching" POSITION="bottom_or_right" ID="ID_224641510" CREATED="1771579386803" MODIFIED="1771579390615"/>
+<node TEXT="13. Choosing the Right Estimator" POSITION="bottom_or_right" ID="ID_1084658763" CREATED="1771579390779" MODIFIED="1771579398535"/>
+<node TEXT="14. External Resources, Videos and Talks" POSITION="bottom_or_right" ID="ID_985626232" CREATED="1771579398716" MODIFIED="1771579409038"/>
+<node TEXT="API Sections" POSITION="top_or_left" ID="ID_839220462" CREATED="1771579879787" MODIFIED="1771580051802">
+<node TEXT="sklearn.linear_model" ID="ID_510399683" CREATED="1771579889218" MODIFIED="1771579903686">
+<node TEXT="LogisticRegression" ID="ID_603814128" CREATED="1771579907667" MODIFIED="1771579916574"/>
+<node TEXT="LogisticRegressionCV" ID="ID_1261563869" CREATED="1771579916730" MODIFIED="1771579922351"/>
+<node TEXT="PassiveAggressiveClassifier" ID="ID_1299939253" CREATED="1771579922675" MODIFIED="1771579928795"/>
+<node TEXT="Perceptron" ID="ID_1920478381" CREATED="1771579929305" MODIFIED="1771579933469"/>
+<node TEXT="Ridgeclassifier" ID="ID_1987631191" CREATED="1771579933627" MODIFIED="1771579940860"/>
+<node TEXT="RidgeClassifierCV" ID="ID_239384472" CREATED="1771579941491" MODIFIED="1771579946502"/>
+<node TEXT="SGDClassifier" ID="ID_1020374482" CREATED="1771579947052" MODIFIED="1771579951396"/>
+<node TEXT="SGDOneClassSVM" ID="ID_1085613170" CREATED="1771579951963" MODIFIED="1771579957742"/>
+<node TEXT="LinearRegression" ID="ID_1453621060" CREATED="1771579958331" MODIFIED="1771579964070"/>
+</node>
+</node>
+</node>
+</map>
